f0e118b5b314004a99ebb023904c729efbc3f04c
tufik <tufikhasan05@gmail.com>
2023-05-07 09:18:21 +0600

    Route setup

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get( '/', function () {
     return view( 'welcome' );
 } );
+Route::get( '/about', function () {
+    echo "I am about page";
+} );
+Route::get( '/contact', function () {
+    return view( 'contact' );
+} );
