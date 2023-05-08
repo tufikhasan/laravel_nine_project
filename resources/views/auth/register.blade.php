0d8de4e57e004db7a84892b180a671f1e75f0536
tufik <tufikhasan05@gmail.com>
2023-05-08 15:53:11 +0600

    Customize Login Page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -30,8 +30,7 @@
                         <div class="text-center mt-4">
                             <div class="mb-3">
                                 <a href="{{url('/')}}" class="auth-logo">
-                                    <img src="{{asset('backend/assets/images/logo-dark.png')}}" height="30" class="logo-dark mx-auto" alt="">
-                                    <img src="{{asset('backend/assets/images/logo-light.png')}}" height="30" class="logo-light mx-auto" alt="">
+                                    <img src="{{asset('backend/assets/images/logo-dark.png')}}" height="30" class="logo-dark mx-auto" alt="Logo">
                                 </a>
                             </div>
                         </div>
