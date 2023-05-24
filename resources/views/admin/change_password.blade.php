ad313fb4b7f305e55d6f3c9006bee0f07500548d
Towfik Hasan <tufikhasan05@gmail.com>
2023-05-24 12:33:07 +0600

    Front-end setup

--- resources/views/admin/change_password.blade.php
+++ resources/views/admin/change_password.blade.php
@@ -10,7 +10,7 @@
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
                                 <li class="breadcrumb-item"><a href="javascript: void(0);">Upcube</a></li>
-                                <li class="breadcrumb-item active">Change Ppassword</li>
+                                <li class="breadcrumb-item active">Change Password</li>
                             </ol>
                         </div>
                     </div>
