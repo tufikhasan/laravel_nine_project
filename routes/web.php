3e7d79fe37eb9493c6dc1d156b2391836f0f229b
Towfik Hasan <tufikhasan05@gmail.com>
2023-06-20 00:22:30 +0600

    Route authentication using auth middleware

--- routes/web.php
+++ routes/web.php
@@ -11,20 +11,6 @@ use App\Http\Controllers\PortfolioController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get( '/', function () {
-    return view( 'frontend.index' );
-} )->name( 'home.page' );
-
-//Admin Controller
-Route::controller( AdminController::class )->group( function () {
-    Route::get( '/admin/logout', 'destroy' )->name( 'admin.logout' );
-    Route::get( '/admin/profile', 'Profile' )->name( 'admin.profile' );
-    Route::get( '/edit/profile', 'editProfile' )->name( 'edit.profile' );
-    Route::post( '/update/profile', 'updateProfile' )->name( 'update.profile' );
-    Route::get( '/change/password', 'changePassword' )->name( 'change.password' );
-    Route::post( '/change/password', 'updatePassword' )->name( 'update.password' );
-} );
-
 Route::get( '/dashboard', function () {
     return view( 'admin.index' );
 } )->middleware( ['auth', 'verified'] )->name( 'dashboard' );
@@ -35,78 +21,95 @@ Route::middleware( 'auth' )->group( function () {
     Route::delete( '/profile', [ProfileController::class, 'destroy'] )->name( 'profile.destroy' );
 } );
 
-//Home Slide Controller
-Route::controller( HomeSlideController::class )->group( function () {
-    Route::get( '/home/slide', 'homeSlide' )->name( 'home.slide' );
-    Route::post( '/home/slide/{id}', 'homeSlideUpdate' )->name( 'update.slide' );
-} );
+Route::get( '/', function () {
+    return view( 'frontend.index' );
+} )->name( 'home.page' );
 
-//About Slide All route
-Route::controller( AboutControllers::class )->group( function () {
-    Route::get( '/about/info', 'aboutInfo' )->name( 'about.info' );
-    Route::post( '/update/about/{id}', 'updateAbout' )->name( 'update.about' );
-    Route::get( '/about', 'aboutPage' )->name( 'about.page' );
+Route::middleware( ['auth'] )->group( function () {
 
-    Route::get( '/multi/image', 'multiImage' )->name( 'multi.image' );
-    Route::post( '/multi/image', 'storeMultiImage' )->name( 'multi.image' );
-    Route::get( '/all/multi', 'allMultiImage' )->name( 'all.multi' );
+    //Admin Controller
+    Route::controller( AdminController::class )->group( function () {
+        Route::get( '/admin/logout', 'destroy' )->name( 'admin.logout' );
+        Route::get( '/admin/profile', 'Profile' )->name( 'admin.profile' );
+        Route::get( '/edit/profile', 'editProfile' )->name( 'edit.profile' );
+        Route::post( '/update/profile', 'updateProfile' )->name( 'update.profile' );
+        Route::get( '/change/password', 'changePassword' )->name( 'change.password' );
+        Route::post( '/change/password', 'updatePassword' )->name( 'update.password' );
+    } );
 
-    Route::get( '/multi/image/{id}', 'editMultiImage' )->name( 'edit.multi.image' );
-    Route::post( '/multi/image/{id}', 'updateMultiImage' )->name( 'update.multi.image' );
-    Route::get( '/delete/multi/image/{id}', 'deleteMultiImage' )->name( 'delete.multi.image' );
+    //Home Slide Controller
+    Route::controller( HomeSlideController::class )->group( function () {
+        Route::get( '/home/slide', 'homeSlide' )->name( 'home.slide' );
+        Route::post( '/home/slide/{id}', 'homeSlideUpdate' )->name( 'update.slide' );
+    } );
 
-} );
+    //About Slide All route
+    Route::controller( AboutControllers::class )->group( function () {
+        Route::get( '/about/info', 'aboutInfo' )->name( 'about.info' );
+        Route::post( '/update/about/{id}', 'updateAbout' )->name( 'update.about' );
+        Route::get( '/about', 'aboutPage' )->name( 'about.page' )->withoutMiddleware( 'auth' );
 
-//Portfolio all route
-Route::controller( PortfolioController::class )->group( function () {
-    Route::get( '/all/portfolios', 'allPortfolio' )->name( 'all.portfolio' );
-    Route::get( '/add/portfolio', 'addPortfolio' )->name( 'add.portfolio' );
-    Route::post( '/add/portfolio', 'storePortfolio' )->name( 'store.portfolio' );
-    Route::get( '/edit/portfolio/{id}', 'editPortfolio' )->name( 'edit.portfolio' );
-    Route::patch( '/edit/portfolio/{id}', 'updatePortfolio' )->name( 'update.portfolio' );
-    Route::get( '/edit/portfolio/{id}', 'editPortfolio' )->name( 'edit.portfolio' );
-    Route::get( '/delete/portfolio/{id}', 'deletePortfolio' )->name( 'delete.portfolio' );
-    // Route::delete( '/delete/portfolio/{id}', 'deletePortfolio' )->name( 'delete.portfolio' );
-    Route::get( '/portfolio/details/{id}', 'detailsPortfolio' )->name( 'portfolio.details' );
-    Route::get( '/portfolios', 'portfolioPage' )->name( 'portfolio.page' );
-} );
+        Route::get( '/multi/image', 'multiImage' )->name( 'multi.image' );
+        Route::post( '/multi/image', 'storeMultiImage' )->name( 'multi.image' );
+        Route::get( '/all/multi', 'allMultiImage' )->name( 'all.multi' );
 
-//Blog Category all route
-Route::controller( BlogCategoryController::class )->group( function () {
-    Route::get( '/all/category', 'allCategory' )->name( 'all.category' );
-    Route::get( '/add/category', 'addCategory' )->name( 'add.category' );
-    Route::post( '/add/category', 'storeCategory' )->name( 'store.category' );
-    Route::get( '/edit/category/{id}', 'editCategory' )->name( 'edit.category' );
-    Route::patch( '/edit/category/{id}', 'updateCategory' )->name( 'update.category' );
-    Route::get( '/delete/category/{id}', 'deleteCategory' )->name( 'delete.category' );
-} );
+        Route::get( '/multi/image/{id}', 'editMultiImage' )->name( 'edit.multi.image' );
+        Route::post( '/multi/image/{id}', 'updateMultiImage' )->name( 'update.multi.image' );
+        Route::get( '/delete/multi/image/{id}', 'deleteMultiImage' )->name( 'delete.multi.image' );
 
-//Blog all route
-Route::controller( BlogController::class )->group( function () {
-    Route::get( '/all/blog', 'allBlog' )->name( 'all.blog' );
-    Route::get( '/add/blog', 'addBlog' )->name( 'add.blog' );
-    Route::post( '/add/blog', 'storeBlog' )->name( 'store.blog' );
-    Route::get( '/edit/blog/{id}', 'editBlog' )->name( 'edit.blog' );
-    Route::patch( '/edit/blog/{id}', 'updateBlog' )->name( 'update.blog' );
-    Route::get( '/delete/blog/{id}', 'deleteBlog' )->name( 'delete.blog' );
-    Route::get( '/blog/details/{id}', 'blogDetails' )->name( 'blog.details' );
-    Route::get( '/category/blogs/{id}', 'categoryBlogs' )->name( 'category.blogs' );
-    Route::get( '/blogs', 'blogPage' )->name( 'blog.page' );
-} );
+    } );
 
-//Footer all route
-Route::controller( FooterController::class )->group( function () {
-    //Seeding footer demo data command=> php artisan db:seed --class=FooterSeeder
-    Route::get( '/footer/setup', 'footerSetup' )->name( 'footer.setup' );
-    Route::patch( '/footer/setup/{id}', 'updateFooter' )->name( 'update.footer' );
-} );
+    //Portfolio all route
+    Route::controller( PortfolioController::class )->group( function () {
+        Route::get( '/all/portfolios', 'allPortfolio' )->name( 'all.portfolio' );
+        Route::get( '/add/portfolio', 'addPortfolio' )->name( 'add.portfolio' );
+        Route::post( '/add/portfolio', 'storePortfolio' )->name( 'store.portfolio' );
+        Route::get( '/edit/portfolio/{id}', 'editPortfolio' )->name( 'edit.portfolio' );
+        Route::patch( '/edit/portfolio/{id}', 'updatePortfolio' )->name( 'update.portfolio' );
+        Route::get( '/delete/portfolio/{id}', 'deletePortfolio' )->name( 'delete.portfolio' );
+        // Route::delete( '/delete/portfolio/{id}', 'deletePortfolio' )->name( 'delete.portfolio' );
+        Route::get( '/portfolio/details/{id}', 'detailsPortfolio' )->name( 'portfolio.details' )->withoutMiddleware( 'auth' );
+        Route::get( '/portfolios', 'portfolioPage' )->name( 'portfolio.page' )->withoutMiddleware( 'auth' );
+    } );
 
-//Contact all route
-Route::controller( ContactController::class )->group( function () {
-    Route::get( '/all/contact', 'allContact' )->name( 'all.contact' );
-    Route::get( '/contact', 'contactPage' )->name( 'contact.me' );
-    Route::post( '/contact/page', 'contactUs' )->name( 'contact.us' );
-    Route::get( '/delete/contact/{id}', 'deleteContact' )->name( 'delete.contact' );
+    //Blog Category all route
+    Route::controller( BlogCategoryController::class )->group( function () {
+        Route::get( '/all/category', 'allCategory' )->name( 'all.category' );
+        Route::get( '/add/category', 'addCategory' )->name( 'add.category' );
+        Route::post( '/add/category', 'storeCategory' )->name( 'store.category' );
+        Route::get( '/edit/category/{id}', 'editCategory' )->name( 'edit.category' );
+        Route::patch( '/edit/category/{id}', 'updateCategory' )->name( 'update.category' );
+        Route::get( '/delete/category/{id}', 'deleteCategory' )->name( 'delete.category' );
+    } );
+
+    //Blog all route
+    Route::controller( BlogController::class )->group( function () {
+        Route::get( '/all/blog', 'allBlog' )->name( 'all.blog' );
+        Route::get( '/add/blog', 'addBlog' )->name( 'add.blog' );
+        Route::post( '/add/blog', 'storeBlog' )->name( 'store.blog' );
+        Route::get( '/edit/blog/{id}', 'editBlog' )->name( 'edit.blog' );
+        Route::patch( '/edit/blog/{id}', 'updateBlog' )->name( 'update.blog' );
+        Route::get( '/delete/blog/{id}', 'deleteBlog' )->name( 'delete.blog' );
+        Route::get( '/blog/details/{id}', 'blogDetails' )->name( 'blog.details' )->withoutMiddleware( 'auth' );
+        Route::get( '/category/blogs/{id}', 'categoryBlogs' )->name( 'category.blogs' )->withoutMiddleware( 'auth' );
+        Route::get( '/blogs', 'blogPage' )->name( 'blog.page' )->withoutMiddleware( 'auth' );
+    } );
+
+    //Footer all route
+    Route::controller( FooterController::class )->group( function () {
+        //Seeding footer demo data command=> php artisan db:seed --class=FooterSeeder
+        Route::get( '/footer/setup', 'footerSetup' )->name( 'footer.setup' );
+        Route::patch( '/footer/setup/{id}', 'updateFooter' )->name( 'update.footer' );
+    } );
+
+    //Contact all route
+    Route::controller( ContactController::class )->group( function () {
+        Route::get( '/all/contact', 'allContact' )->name( 'all.contact' );
+        Route::post( '/contact/page', 'contactUs' )->name( 'contact.us' );
+        Route::get( '/delete/contact/{id}', 'deleteContact' )->name( 'delete.contact' );
+        Route::get( '/contact', 'contactPage' )->name( 'contact.me' )->withoutMiddleware( 'auth' );
+    } );
+
 } );
 
 require __DIR__ . '/auth.php';
